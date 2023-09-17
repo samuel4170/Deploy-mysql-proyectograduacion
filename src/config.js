f45fa3ac1d11f3d6a2866ce71d68018986715723
samuel rolando martinez lopez <smartinezl2@miumg.edu.gt>
2023-09-17 12:33:36 -0600

    0do

--- src/config.js
+++ src/config.js
@@ -3,10 +3,12 @@ const { config } = require('dotenv');
 config()
 
 module.exports = {
-    PORT: process.env.PORT || 8080,
     DB_HOST: process.env.DB_HOST || "localhost",
     DB_USER: process.env.DB_USER || "root",
     DB_PASSWORD: process.env.DB_PASSWORD || "1234",
     DB_DATABASE: process.env.DB_DATABASE || "Clinica",
     DB_PORT: process.env.DB_PORT || 3306,
 };
+
+
+
