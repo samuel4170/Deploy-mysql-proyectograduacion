44b9c908015392263955f1c1d464969f773c4c2f
samuel rolando martinez lopez <smartinezl2@miumg.edu.gt>
2023-09-17 14:30:47 -0600

    casi45

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const sequelize = require("./src/db"); // Ruta corregida para importar sequelize
 const app = express();
 
-const { PORT } = require("./src/config"); // Ruta corregida para importar la configuración
+const { PORT } = require("../config/db.config.js"); // Ruta corregida para importar la configuración
 
 //dep
 app.use(cors());
