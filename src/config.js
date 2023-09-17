baa2b6482356df245a48141f3b101921133fdab3
samuel rolando martinez lopez <smartinezl2@miumg.edu.gt>
2023-09-17 14:09:21 -0600

    casi11

--- src/config.js
+++ src/config.js
@@ -3,6 +3,7 @@ const { config } = require('dotenv');
 config()
 
 module.exports = {
+    PORT: process.env.PORT || 8080,
     DB_HOST: process.env.DB_HOST || "localhost",
     DB_USER: process.env.DB_USER || "root",
     DB_PASSWORD: process.env.DB_PASSWORD || "1234",
--- src/db.js
+++ src/db.js
@@ -1,6 +1,7 @@
 require('dotenv').config(); // Cargar las variables de entorno desde el archivo .env
 
 module.exports = {
+  PORT: process.env.PORT || 8080,
   DB_HOST: process.env.DB_HOST || "localhost",
   DB_USER: process.env.DB_USER || "root",
   DB_PASSWORD: process.env.DB_PASSWORD || "1234",
